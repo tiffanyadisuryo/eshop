427995cd420ec8847ca2e47a3753766dffa2c16b
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2024-02-04 20:21:17 +0700

    fixed bug on controller

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -1,2 +1,10 @@
-package id.ac.ui.cs.advprog.eshop.model;public class Product {
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter @Setter
+public class Product {
+    private String productId;
+    private String productName;
+    private int productQuantity;
 }
