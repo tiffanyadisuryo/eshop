12c0f27da672ffe91717c91287cb08343ec156f6
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2024-02-13 16:11:26 +0700

    from 50ish % to 71%

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceTest.java
@@ -1,21 +1,14 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
-import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
