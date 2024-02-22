91a68928d9490331e9279c2010b296099c560bdd
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2024-02-22 13:18:44 +0700

    added Car.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -1,2 +1,10 @@
-package id.ac.ui.cs.advprog.eshop.model;public class Car {
+package id.ac.ui.cs.advprog.eshop.model;
+import lombok.Getter;
+import lombok.Setter;
+@Getter @Setter
+public class Car {
+    private String carId;
+    private String carName;
+    private String carColor;
+    private int carQuantity;
 }
